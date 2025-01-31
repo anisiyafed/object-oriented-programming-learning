acb3aca34c32f4107f3ecd085da7dbde0abeed7d
Anisiya Fedoseeva <capykaze@gmail.com>
2025-01-31 23:54:57 +0000

    savings account with interest

--- projects/banking_system.py
+++ projects/banking_system.py
@@ -16,7 +16,13 @@ class Account:
         return f"Account {self._account_number} ({self.owner}): Balance ${self._balance:.2f}"
 
 class SavingsAccount(Account):
-    pass
+    def __init__(self, account_number: str, owner: str, balance: float = 0.0, interest_rate: float = 0.02):
+        super().__init__(account_number, owner, balance)
+        self.interest_rate = interest_rate
+
+    def apply_interest(self):
+        interest = self._balance * self.interest_rate
+        self._balance += interest
 
 class CheckingAccount(Account):
     pass
