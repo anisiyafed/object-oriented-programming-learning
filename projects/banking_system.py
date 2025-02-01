a1143f8857f42549d51eb3f459b75570d8f1401f
Anisiya Fedoseeva <capykaze@gmail.com>
2025-02-01 21:14:23 +0000

    raising errors instead of using abs

--- projects/banking_system.py
+++ projects/banking_system.py
@@ -5,11 +5,15 @@ class Account:
         self._balance = balance
 
     def deposit(self, amount: float):
-        amount = abs(amount)
+        if amount <= 0:
+            raise ValueError("Deposit amount must be positive.")
         self._balance += amount
 
     def withdraw(self, amount: float):
-        amount = abs(amount)
+        if amount <= 0:
+            raise ValueError("Withdrawal amount must be positive.")
+        if amount > self._balance:
+            raise ValueError("Insufficient funds!")
         self._balance -= amount
 
     def __str__(self):
